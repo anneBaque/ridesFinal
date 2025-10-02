cb59cea8beebb1c93db6f0fd9ece3a9585d23b75
mleon019 <mleon019@ikasle.ehu.eus>
2025-10-02 20:26:12 +0200

    caja negra mock

--- Rides24/src/test/java/testComprobarDriver/ComprobarDriverMockBlackTest.java
+++ Rides24/src/test/java/testComprobarDriver/ComprobarDriverMockBlackTest.java
@@ -19,49 +19,53 @@ import org.mockito.MockitoAnnotations;
 
 import dataAccess.DataAccess;
 import domain.Driver;
+import domain.Reservation;
 
-public class ComprobarDriverMockWhiteTest {
+public class ComprobarDriverMockBlackTest {
 
-    static DataAccess sut;
-    
-    protected MockedStatic<Persistence> persistenceMock;
+	static DataAccess sut;
+	
+	protected MockedStatic<Persistence> persistenceMock;
 
-    @Mock
-    protected  EntityManagerFactory entityManagerFactory;
-    @Mock
-    protected  EntityManager db;
-    @Mock
+	@Mock
+	protected  EntityManagerFactory entityManagerFactory;
+	@Mock
+	protected  EntityManager db;
+	@Mock
     protected  EntityTransaction  et;
-    
-    @Mock 
-    TypedQuery<Driver> typeQueryDrivers;
-    
+	
+	@Mock 
+	TypedQuery<Reservation> typeQueryReservations;
+	
 
-    @Before
+	@Before
     public  void init() {
         MockitoAnnotations.openMocks(this);
         persistenceMock = Mockito.mockStatic(Persistence.class);
-        persistenceMock.when(() -> Persistence.createEntityManagerFactory(Mockito.any()))
+		persistenceMock.when(() -> Persistence.createEntityManagerFactory(Mockito.any()))
         .thenReturn(entityManagerFactory);
         
         Mockito.doReturn(db).when(entityManagerFactory).createEntityManager();
-        Mockito.doReturn(et).when(db).getTransaction();
-        sut=new DataAccess(db);
+		Mockito.doReturn(et).when(db).getTransaction();
+	    sut=new DataAccess(db);
     }
-
-    @After 
-    public void end() {
-        persistenceMock.close();
+	
+	@After
+    public  void tearDown() {
+		persistenceMock.close();
     }
-
+	
 	@Test
 	public void test1() {
 		System.out.println("Test1");
 		String email = "driver";
+		String contra = "123";
+		Driver driver = new Driver(email, email);
+		driver.setContraseña(contra);
 		sut.open();
-		Mockito.when(db.find(Driver.class, email)).thenReturn(null);
-		boolean existe = sut.comprobarDriver(email, "123");
-		assertTrue(!existe);
+		Mockito.when(db.find(Driver.class, email)).thenReturn(driver);
+		boolean existe = sut.comprobarDriver(email, contra);
+		assertTrue(existe);
 		sut.close();
 	}
 	
@@ -78,21 +82,33 @@ public class ComprobarDriverMockWhiteTest {
 		assertTrue(!existe);
 		sut.close();
 	}
-
+	
 	@Test
 	public void test3() {
 		System.out.println("Test3");
+		sut.open();
 		String email = "driver";
 		String contra = "123";
 		Driver driver = new Driver(email, email);
 		driver.setContraseña(contra);
-		sut.open();
-		Mockito.when(db.find(Driver.class, email)).thenReturn(driver);
-		boolean existe = sut.comprobarDriver(email, contra);
-		assertTrue(existe);
+		Mockito.when(db.find(Driver.class, null)).thenReturn(null);
+		boolean existe = sut.comprobarDriver(null, contra);
+		assertTrue(!existe);
 		sut.close();
 	}
-
 	
-
+	@Test
+	public void test4() {
+		System.out.println("Test4");
+		sut.open();
+		String email = "driver";
+		String contra = "123";
+		Driver driver = new Driver(email, email);
+		driver.setContraseña(contra);
+		Mockito.when(db.find(Driver.class, email)).thenReturn(driver);
+		boolean existe = sut.comprobarDriver(email, null);
+		assertTrue(!existe);
+		sut.close();
+	}
+	
 }
