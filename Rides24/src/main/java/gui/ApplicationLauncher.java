5d7d6ba6246d545fd75c18a67013c6d5f19eed14
mleon019 <mleon019@ikasle.ehu.eus>
2025-10-11 16:19:56 +0200

    long method refactor

--- Rides24/src/main/java/gui/ApplicationLauncher.java
+++ Rides24/src/main/java/gui/ApplicationLauncher.java
@@ -27,85 +27,80 @@ public class ApplicationLauncher {
 	public static void main(String[] args) {
 
 		ConfigXML c=ConfigXML.getInstance();
-	
-		System.out.println(c.getLocale());
 		
-		Locale.setDefault(new Locale(c.getLocale()));
-		
-		System.out.println("Locale: "+Locale.getDefault());
-		
-	    Driver driver=new Driver("driver3@gmail.com","Test Driver");
+		setUpLocale(c);
 
-		
+	    if(c.isDatabaseInitialized()) {
+			cleanOldDbImages(c);
+		}
+	    
+	    Driver driver=new Driver("driver3@gmail.com","Test Driver");
 		MainGUI a=new MainGUI(driver);
 		a.setVisible(true);
-		if(c.isDatabaseInitialized()) {
-			 Path carpeta = Paths.get("imagenes");
-			 //Eliminamos las imagenes de la base de datos antigua
-		        if (Files.exists(carpeta) && Files.isDirectory(carpeta)) {
-					try (DirectoryStream<Path> archivos = Files.newDirectoryStream(carpeta)) { //iterar sobre el directorio
-						for (Path archivo : archivos) {
-							try{
-								Files.delete(archivo);
-							} catch (IOException e) {
-								System.err.println("Error al borrar el archivo: " + archivo + " -> " + e.getMessage());
-                			}
-						}
+		
+		initializeBusinessLogic(c, a);
+
+	}
+	
+	public static void setUpLocale(ConfigXML config) {
+		System.out.println(config.getLocale());
+		Locale.setDefault(new Locale(config.getLocale()));
+		System.out.println("Locale: "+Locale.getDefault());
+	}
+	
+	public static void cleanOldDbImages(ConfigXML config) {
+		Path carpeta = Paths.get("imagenes");
+		//Eliminamos las imagenes de la base de datos antigua
+	    if (Files.exists(carpeta) && Files.isDirectory(carpeta)) {
+	    	try (DirectoryStream<Path> archivos = Files.newDirectoryStream(carpeta)) { //iterar sobre el directorio
+	    		for (Path archivo : archivos) {
+					try{
+						Files.delete(archivo);
 					} catch (IOException e) {
-            			System.err.println("Error al leer " + carpeta + ": " + e.getMessage());
-        			}
+						System.err.println("Error al borrar el archivo: " + archivo + " -> " + e.getMessage());
+           			}
+				}
+			} catch (IOException e) {
+       			System.err.println("Error al leer " + carpeta + ": " + e.getMessage());
+   			}
 
-        			try {
-            			Files.delete(carpeta); // Eliminar la carpeta después de vaciarla
-        			} catch (IOException e) {
-            			System.err.println("Error al borrar la carpeta: " + carpeta + " -> " + e.getMessage());
-        			}
-				}	
+   			try {
+       			Files.delete(carpeta); // Eliminar la carpeta después de vaciarla
+   			} catch (IOException e) {
+       			System.err.println("Error al borrar la carpeta: " + carpeta + " -> " + e.getMessage());
+   			}
 		}
-
+	}
+	
+	public static void initializeBusinessLogic(ConfigXML c, MainGUI a) {
 		try {
-			
 			BLFacade appFacadeInterface;
 			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
 			
 			if (c.isBusinessLogicLocal()) {
-			
 				DataAccess da= new DataAccess();
 				appFacadeInterface=new BLFacadeImplementation(da);
-			
 			}	
 			
-			
 			else { //If remote
-				
-				 String serviceName= "http://"+c.getBusinessLogicNode() +":"+ c.getBusinessLogicPort()+"/ws/"+c.getBusinessLogicName()+"?wsdl";
-				 
+				String serviceName= "http://"+c.getBusinessLogicNode() +":"+ c.getBusinessLogicPort()+"/ws/"+c.getBusinessLogicName()+"?wsdl";
 				URL url = new URL(serviceName);
-
 		 
 		        //1st argument refers to wsdl document above
 				//2nd argument is service name, refer to wsdl document above
 		        QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
-		 
 		        Service service = Service.create(url, qname);
-
-		         appFacadeInterface = service.getPort(BLFacade.class);
+		        appFacadeInterface = service.getPort(BLFacade.class);
 			} 
-			
+	
 			MainGUI.setBussinessLogic(appFacadeInterface);
 
-		
-
-			
 		}catch (Exception e) {
 			a.jLabelSelectOption.setText("Error: "+e.toString());
 			a.jLabelSelectOption.setForeground(Color.RED);	
 			
 			System.out.println("Error in ApplicationLauncher: "+e.toString());
 		}
-		//a.pack();
-
-
 	}
 
 }
